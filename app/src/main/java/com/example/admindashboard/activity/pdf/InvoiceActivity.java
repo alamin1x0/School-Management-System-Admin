f8371adac4aa8d203ad99baf79fa90641a65a702
alamin1x0 <msasakibalamin@gmail.com>
2022-03-07 14:17:15 +0600

    Admin Dashboard

--- app/src/main/java/com/example/admindashboard/activity/pdf/InvoiceActivity.java
+++ app/src/main/java/com/example/admindashboard/activity/pdf/InvoiceActivity.java
@@ -16,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.admindashboard.R;
 import com.google.firebase.database.DataSnapshot;
@@ -30,6 +31,8 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import es.dmoral.toasty.Toasty;
 
 public class InvoiceActivity extends AppCompatActivity {
 
@@ -76,6 +79,26 @@ public class InvoiceActivity extends AppCompatActivity {
         saveAndPrintButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String name = editTextName.getText().toString();
+                String price = editTextQty.getText().toString();
+
+                if (name.isEmpty()) {
+                    editTextName.setError("গ্রাহকের নাম");
+                    editTextName.requestFocus();
+                    Toasty.error(InvoiceActivity.this, "গ্রাহকের নাম", Toasty.LENGTH_SHORT).show();
+                    return;
+                }
+                if (price.isEmpty()) {
+                    editTextQty.setError("পরিমান");
+                    editTextQty.requestFocus();
+                    Toasty.error(InvoiceActivity.this, "পরিমান", Toasty.LENGTH_SHORT).show();
+                    return;
+                } else {
+
+                }
+
+
                 pdfModel.invoiceNo = invoiceNo + 1;
                 pdfModel.customerName = String.valueOf(editTextName.getText());
                 pdfModel.date = new Date().getTime();
@@ -86,6 +109,8 @@ public class InvoiceActivity extends AppCompatActivity {
                 myRef.child(String.valueOf(invoiceNo + 1)).setValue(pdfModel);
 
                 printPDF();
+
+                Toasty.success(InvoiceActivity.this, "ইনভয়েস সেভ এবং প্রিন্ট সফল হয়েছে", Toasty.LENGTH_SHORT).show();
             }
         });
 
@@ -112,22 +137,22 @@ public class InvoiceActivity extends AppCompatActivity {
         paint.setTextSize(15.5f);
         paint.setColor(Color.rgb(0, 50, 250));
 
-        canvas.drawText("Developer Al-Amin", 20, 20, paint);
+        canvas.drawText("শ্যামপুর উচ্চ বিদ্যালয়", 20, 20, paint);
         paint.setTextSize(8.5f);
-        canvas.drawText("MR. No. 2, Joypurhat", 20, 40, paint);
-        canvas.drawText("Sakib 901253", 20, 55, paint);
+        canvas.drawText("স্থাপন: ১৯৬৪ সাল  ", 20, 40, paint);
+        canvas.drawText("পুরানাপইল, জয়পুরহাট সাদর", 20, 55, paint);
         forLinePaint.setStyle(Paint.Style.STROKE);
         forLinePaint.setPathEffect(new DashPathEffect(new float[]{5, 5}, 0));
         forLinePaint.setStrokeWidth(2);
         canvas.drawLine(20, 65, 230, 65, forLinePaint);
 
-        canvas.drawText("Customer Name: " + editTextName.getText(), 20, 80, paint);
+        canvas.drawText("শিক্ষার্থীর নাম: " + editTextName.getText(), 20, 80, paint);
         canvas.drawLine(20, 90, 230, 90, forLinePaint);
 
-        canvas.drawText("Purchase: ", 20, 105, paint);
+        canvas.drawText("ক্রয়: ", 20, 105, paint);
 
         canvas.drawText(spinner.getSelectedItem().toString(), 20, 135, paint);
-        canvas.drawText(editTextQty.getText() + " Pice", 120, 135, paint);
+        canvas.drawText(editTextQty.getText() + "পিচ ", 120, 135, paint);
 
         double amount = itemPrice[spinner.getSelectedItemPosition()] * Double.parseDouble(editTextQty.getText().toString());
         paint.setTextAlign(Paint.Align.RIGHT);
@@ -135,7 +160,7 @@ public class InvoiceActivity extends AppCompatActivity {
         paint.setTextAlign(Paint.Align.LEFT);
 
         canvas.drawText("+%", 20, 175, paint);
-        canvas.drawText("Tax 5%", 120, 175, paint);
+        canvas.drawText("ভ্যাট 5%", 120, 175, paint);
         paint.setTextAlign(Paint.Align.RIGHT);
         canvas.drawText(decimalFormat.format(amount * 0 / 100), 230, 175, paint);
         paint.setTextAlign(Paint.Align.LEFT);
@@ -143,23 +168,23 @@ public class InvoiceActivity extends AppCompatActivity {
         canvas.drawLine(20, 210, 230, 210, forLinePaint);
 
         paint.setTextSize(10f);
-        canvas.drawText("Total", 120, 225, paint);
+        canvas.drawText("মোট ", 120, 225, paint);
         paint.setTextAlign(Paint.Align.RIGHT);
         canvas.drawText(decimalFormat.format((amount * 5 / 100) + amount), 230, 225, paint);
         paint.setTextAlign(Paint.Align.LEFT);
         paint.setTextSize(8.5f);
 
-        canvas.drawText("Date: " + datePatternformat.format(new Date().getTime()), 20, 260, paint);
+        canvas.drawText("তারিখ: " + datePatternformat.format(new Date().getTime()), 20, 260, paint);
         canvas.drawText(String.valueOf(invoiceNo + 1), 20, 275, paint);
-        canvas.drawText("Payment Method Cash", 20, 290, paint);
+        canvas.drawText("পেমেন্ট পদ্ধতি নগদ", 20, 290, paint);
 
 
         paint.setTextAlign(Paint.Align.CENTER);
         paint.setTextSize(12f);
-        canvas.drawText("Thank you!", canvas.getWidth() / 2, 320, paint);
+        canvas.drawText("ধন্যবাদ!", canvas.getWidth() / 2, 320, paint);
 
         mypdfDocument.finishPage(myPage);
-        File file = new File(this.getExternalFilesDir("/"), "Developer alamin.pdf");
+        File file = new File(this.getExternalFilesDir("/"), "শ্যামপুর উচ্চ বিদ্যালয়.pdf");
 
         try {
             mypdfDocument.writeTo(new FileOutputStream(file));
@@ -172,19 +197,16 @@ public class InvoiceActivity extends AppCompatActivity {
 
     }
 
-
     private void callFindViewById() {
-
         saveAndPrintButton = findViewById(R.id.btnSaveAndPrint);
         printbutton = findViewById(R.id.btnPrint);
         editTextName = findViewById(R.id.editTextName);
         editTextQty = findViewById(R.id.editTextQty);
         spinner = findViewById(R.id.spinner);
 
-        itemList = new String[]{"phone", "laptop"};
+        itemList = new String[]{"ফোন ", "ল্যাপটপ "};
         itemPrice = new double[]{1200.00, 38000.00};
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, itemList);
         spinner.setAdapter(adapter);
-
     }
 }
--- app/src/main/java/com/example/admindashboard/activity/pdf/OldPdfPrintActivity.java
+++ app/src/main/java/com/example/admindashboard/activity/pdf/OldPdfPrintActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.admindashboard.R;
 import com.google.firebase.database.DataSnapshot;
@@ -29,6 +30,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import es.dmoral.toasty.Toasty;
 import ir.androidexception.datatable.DataTable;
 import ir.androidexception.datatable.model.DataTableHeader;
 import ir.androidexception.datatable.model.DataTableRow;
@@ -66,12 +68,18 @@ public class OldPdfPrintActivity extends AppCompatActivity {
         oldPrintEditText = findViewById(R.id.oldPrintEditText);
 
         header = new DataTableHeader.Builder()
-                .item("Invoice No.", 5)
-                .item("Customer name", 5)
-                .item("Date", 5)
-                .item("Time", 5)
-                .item("Amount", 5)
+                //.item("Invoice No.", 5)
+                .item("ইনভইচ নং.", 5)
+                //.item("Customer name", 5)
+                .item("শিক্ষার্থীর নাম: ", 5)
+                //.item("Date", 5)
+                .item("ডেট", 5)
+                //.item("Time", 5)
+                .item("সময় ", 5)
+                //.item("Amount", 5)
+                .item("টাকা ", 5)
                 .build();
+
         loadTable();
         listeners();
     }
@@ -80,6 +88,15 @@ public class OldPdfPrintActivity extends AppCompatActivity {
         oldPrintBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String oldprint = oldPrintEditText.getText().toString();
+                if (oldprint.isEmpty()) {
+                    oldPrintEditText.setError("ইনভয়েস নাম্বার");
+                    oldPrintEditText.requestFocus();
+                    Toasty.error(OldPdfPrintActivity.this, "ইনভয়েস নাম্বার", Toasty.LENGTH_SHORT).show();
+                    return;
+                } else {
+
+                }
                 printPdf(oldPrintEditText.getText().toString());
             }
         });
@@ -87,90 +104,94 @@ public class OldPdfPrintActivity extends AppCompatActivity {
 
     private void printPdf(String invoice) {
         retriveRef = FirebaseDatabase.getInstance().getReference().child("Invoice").child(invoice);
+
         retriveRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                invoiceNo = (long) snapshot.child("invoiceNo").getValue();
-                customerName = (String) snapshot.child("customerName").getValue();
-                databaseDate = (long) snapshot.child("date").getValue();
-                fuelType = String.valueOf(snapshot.child("fuelType").getValue());
-                fuelQty = Double.parseDouble(String.valueOf(snapshot.child("fuelQty").getValue()));
-                amount = Double.parseDouble(String.valueOf(snapshot.child("amount").getValue()));
+
+                if (snapshot.getValue() != null) {
+                    invoiceNo = (long) snapshot.child("invoiceNo").getValue();
+                    customerName = (String) snapshot.child("customerName").getValue();
+                    databaseDate = (long) snapshot.child("date").getValue();
+                    fuelType = String.valueOf(snapshot.child("fuelType").getValue());
+                    fuelQty = Double.parseDouble(String.valueOf(snapshot.child("fuelQty").getValue()));
+                    amount = Double.parseDouble(String.valueOf(snapshot.child("amount").getValue()));
 
 
-                @SuppressLint({"NewApi", "LocalSuppress"}) PdfDocument mypdfDocument = new PdfDocument();
-                Paint paint = new Paint();
-                Paint forLinePaint = new Paint();
-                forLinePaint.setColor(Color.rgb(0, 50, 250));
-                @SuppressLint({"NewApi", "LocalSuppress"}) PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(250, 350, 1).create();
-                @SuppressLint({"NewApi", "LocalSuppress"}) PdfDocument.Page myPage = mypdfDocument.startPage(myPageInfo);
-                @SuppressLint({"NewApi", "LocalSuppress"}) Canvas canvas = myPage.getCanvas();
+                    @SuppressLint({"NewApi", "LocalSuppress"}) PdfDocument mypdfDocument = new PdfDocument();
+                    Paint paint = new Paint();
+                    Paint forLinePaint = new Paint();
+                    forLinePaint.setColor(Color.rgb(0, 50, 250));
+                    @SuppressLint({"NewApi", "LocalSuppress"}) PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(250, 350, 1).create();
+                    @SuppressLint({"NewApi", "LocalSuppress"}) PdfDocument.Page myPage = mypdfDocument.startPage(myPageInfo);
+                    @SuppressLint({"NewApi", "LocalSuppress"}) Canvas canvas = myPage.getCanvas();
 
-                paint.setTextSize(15.5f);
-                paint.setColor(Color.rgb(0, 50, 250));
+                    paint.setTextSize(15.5f);
+                    paint.setColor(Color.rgb(0, 50, 250));
 
-                canvas.drawText("Developer Al-Amin", 20, 20, paint);
-                paint.setTextSize(8.5f);
-                canvas.drawText("MR. No. 2, Joypurhat", 20, 40, paint);
-                canvas.drawText("Sakib 901253", 20, 55, paint);
-                forLinePaint.setStyle(Paint.Style.STROKE);
-                forLinePaint.setPathEffect(new DashPathEffect(new float[]{5, 5}, 0));
-                forLinePaint.setStrokeWidth(2);
-                canvas.drawLine(20, 65, 230, 65, forLinePaint);
+                    canvas.drawText("শ্যামপুর উচ্চ বিদ্যালয়", 20, 20, paint);
+                    paint.setTextSize(8.5f);
+                    canvas.drawText("স্থাপন: ১৯৬৪ সাল  ", 20, 40, paint);
+                    canvas.drawText("পুরানাপইল, জয়পুরহাট সাদর", 20, 55, paint);
+                    forLinePaint.setStyle(Paint.Style.STROKE);
+                    forLinePaint.setPathEffect(new DashPathEffect(new float[]{5, 5}, 0));
+                    forLinePaint.setStrokeWidth(2);
+                    canvas.drawLine(20, 65, 230, 65, forLinePaint);
 
-                canvas.drawText("Customer Name: " + customerName, 20, 80, paint);
-                canvas.drawLine(20, 90, 230, 90, forLinePaint);
+                    canvas.drawText("শিক্ষার্থীর নাম : " + customerName, 20, 80, paint);
+                    canvas.drawLine(20, 90, 230, 90, forLinePaint);
 
-                canvas.drawText("Purchase: ", 20, 105, paint);
+                    canvas.drawText("ক্রয়: ", 20, 105, paint);
 
-                canvas.drawText(fuelType, 20, 135, paint);
-                canvas.drawText(fuelQty + " Pice", 120, 135, paint);
+                    canvas.drawText(fuelType, 20, 135, paint);
+                    canvas.drawText(fuelQty + "পিচ ", 120, 135, paint);
 
-                paint.setTextAlign(Paint.Align.RIGHT);
-                canvas.drawText(String.valueOf(decimalFormat.format(amount)), 230, 135, paint);
-                paint.setTextAlign(Paint.Align.LEFT);
+                    paint.setTextAlign(Paint.Align.RIGHT);
+                    canvas.drawText(String.valueOf(decimalFormat.format(amount)), 230, 135, paint);
+                    paint.setTextAlign(Paint.Align.LEFT);
 
-                canvas.drawText("+%", 20, 175, paint);
-                canvas.drawText("Tax 5%", 120, 175, paint);
-                paint.setTextAlign(Paint.Align.RIGHT);
-                canvas.drawText(decimalFormat.format(amount * 0 / 100), 230, 175, paint);
-                paint.setTextAlign(Paint.Align.LEFT);
+                    canvas.drawText("+%", 20, 175, paint);
+                    canvas.drawText("ভ্যাট 5%", 120, 175, paint);
+                    paint.setTextAlign(Paint.Align.RIGHT);
+                    canvas.drawText(decimalFormat.format(amount * 0 / 100), 230, 175, paint);
+                    paint.setTextAlign(Paint.Align.LEFT);
 
-                canvas.drawLine(20, 210, 230, 210, forLinePaint);
+                    canvas.drawLine(20, 210, 230, 210, forLinePaint);
 
-                paint.setTextSize(10f);
-                canvas.drawText("Total", 120, 225, paint);
-                paint.setTextAlign(Paint.Align.RIGHT);
-                canvas.drawText(decimalFormat.format((amount * 5 / 100) + amount), 230, 225, paint);
-                paint.setTextAlign(Paint.Align.LEFT);
-                paint.setTextSize(8.5f);
+                    paint.setTextSize(10f);
+                    canvas.drawText("মোট ", 120, 225, paint);
+                    paint.setTextAlign(Paint.Align.RIGHT);
+                    canvas.drawText(decimalFormat.format((amount * 5 / 100) + amount), 230, 225, paint);
+                    paint.setTextAlign(Paint.Align.LEFT);
+                    paint.setTextSize(8.5f);
 
-                canvas.drawText("Date: " + datePattenFormat.format(new Date(databaseDate).getTime()), 20, 260, paint);
-                canvas.drawText(String.valueOf(invoiceNo), 20, 275, paint);
-                canvas.drawText("Payment Method Cash", 20, 290, paint);
+                    canvas.drawText("তারিখ: " + datePattenFormat.format(new Date(databaseDate).getTime()), 20, 260, paint);
+                    canvas.drawText(String.valueOf(invoiceNo), 20, 275, paint);
+                    canvas.drawText("পেমেন্ট পদ্ধতি নগদ", 20, 290, paint);
 
 
-                paint.setTextAlign(Paint.Align.CENTER);
-                paint.setTextSize(12f);
-                canvas.drawText("Thank you!", canvas.getWidth() / 2, 320, paint);
+                    paint.setTextAlign(Paint.Align.CENTER);
+                    paint.setTextSize(12f);
+                    canvas.drawText("ধন্যবাদ!", canvas.getWidth() / 2, 320, paint);
 
-                mypdfDocument.finishPage(myPage);
-                File file = new File(OldPdfPrintActivity.this.getExternalFilesDir("/"), invoiceNo + "Developer alamin.pdf");
+                    mypdfDocument.finishPage(myPage);
+                    File file = new File(OldPdfPrintActivity.this.getExternalFilesDir("/"), invoiceNo + "শ্যামপুর উচ্চ বিদ্যালয়.pdf");
 
-                try {
-                    mypdfDocument.writeTo(new FileOutputStream(file));
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    try {
+                        mypdfDocument.writeTo(new FileOutputStream(file));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    mypdfDocument.close();
+
+                    Toasty.success(OldPdfPrintActivity.this, "ইনভয়েস প্রিন্ট সফল হয়েছে", Toasty.LENGTH_SHORT).show();
                 }
-
-                mypdfDocument.close();
-
-
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toasty.error(OldPdfPrintActivity.this, "কোন ডাটা পাওয়া যাই নি", Toasty.LENGTH_SHORT).show();
             }
         });
     }
@@ -180,6 +201,7 @@ public class OldPdfPrintActivity extends AppCompatActivity {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                rows.clear();
                 for (DataSnapshot myDataSnapshot : snapshot.getChildren()) {
                     DataTableRow row = new DataTableRow.Builder()
                             .value(String.valueOf(myDataSnapshot.child("invoiceNo").getValue()))
